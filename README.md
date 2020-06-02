79427651ee98388c3a0cbef9d780208f6851ae92
Justine <justinekcn@gmail.com>
2020-06-02 19:41:56 +0800

    Allow multiple digit input.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+View it live [here](https://awayfromhomeplanet.github.io/calculator)!
+
 28 May: 
   A very basic calculator. So far it can only handle single-digit whole numbers and the four basic operators. This is the hardest project I've done so far! Took me really long to come up with a system for handling the operator precedence and being able to display the intermediate results while the user is keying in a long equation. Lots of head scratching and flowcharts but it was really fun!
+
+2 June:
+  Now supports numbers with multiple digits. This required a complete overhaul of the program's logic as all operations were now conducted within the enterOperator function (whereas before, * and / were done in the enterDigit function.) I rewrote the enterOperator code 5 times to make it as elegant (eh I hope) as possible, which was hard because there are so many combinations of conditions and ways of grouping them. Eventually I gave = its own function which cleaned things up a lot. This is a lot harder than I thought and it's just the beginning !!!
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => {
-    number.onclick = enterNumber;
-});
+numbers.forEach(
+    (number) => {number.onclick = enterDigit;}
+);
 
 const operators = document.querySelectorAll(".operator");
-operators.forEach((operator) => {
-    operator.onclick = enterOperator;
-})
+operators.forEach(
+    (operator) => {operator.onclick = enterOperator;}
+);
+
+const equal = document.getElementById("equal");
+equal.onclick = enterEqual;
 
 const del = document.getElementById("delete");
 
@@ -15,51 +18,68 @@ clear.onclick = clearAll;
 
 const result = document.getElementById("result");
 
-let currentNum;
+let skippedNum;
 let prevNum;
-let currentOp;
+let newNum = 0;
+let skippedOp;
 let prevOp;
+let newOp;
+let justPressedEqual = false;
 
 function operate (operator, a, b) {
     switch (operator) {
-        case "add": return a + b;
-        case "subtract": return a - b;
-        case "multiply": return a * b;
         case "divide": return a / b;
+        case "multiply": return a * b;
+        case "subtract": return a - b;
+        case "add": return a + b;
     }
 }
 
-function enterNumber () {
-    switch (currentOp) {
-        case "add":
-        case "subtract":
-            currentNum = +this.textContent;
-            break;
-        case "multiply":
-        case "divide":
-            currentNum = operate(currentOp, currentNum, this.textContent);
-            result.textContent = currentNum;
-            break;
-        default: // first number
-            currentNum = +this.textContent;
-    }
+function enterDigit () {
+    newNum = newNum * 10 + +this.textContent;
+    result.textContent = newNum;
+    justPressedEqual = false;
 }
 
 function enterOperator () {
-    currentOp = this.id;
-    switch (currentOp) {
-        case "add":
-        case "subtract":
-            prevNum = prevOp ? operate(prevOp, prevNum, currentNum) : currentNum;
-            result.textContent = prevNum;
-            prevOp = currentOp;
-            break;
-        case "equal":
-            result.textContent = prevOp ? operate(prevOp, prevNum, currentNum) : currentNum;
-            prevOp = prevNum = "";
+    newOp = this.id;
+    if (!justPressedEqual) {
+        if (type(prevOp) == 1 && type(newOp) == 2) { // a + b *
+            //save a and + for later
+            skippedOp = prevOp; 
+            skippedNum = prevNum;
+            prevNum = newNum;
+        }
+        // a + b +   OR   a * b +   OR   a * b *   OR   a + b =   OR   a * b =
+        else if (prevOp) prevNum = operate(prevOp, prevNum, newNum);
+        // first operator, don't do anything
+        else prevNum = newNum;
+
+        if (skippedOp && type(prevOp) == 2 && type(newOp) == 1) { // c + a * b +   =>   c + ab +
+            prevNum = operate(skippedOp, skippedNum, prevNum); // (c+ab) +
+            skippedOp = skippedNum = 0;
+        }
     }
+    prevOp = newOp;
+    result.textContent = prevNum;
+    newNum = 0;
+    justPressedEqual = false;
+}
+
+type = operator => (operator == "multiply" || operator == "divide") ? 2 : 1;
+
+function enterEqual () {
+    prevNum = operate(prevOp, prevNum, newNum);
+    if (skippedOp) prevNum = operate(skippedOp, skippedNum, prevNum);
+    result.textContent = prevNum;
+    skippedOp = skippedNum = prevOp = newOp = newNum = 0;
+    justPressedEqual = true;
 }
 
 function clearAll () {
-    result.textContent = currentOp = prevOp = currentNum = prevNum = "";
+    result.textContent = skippedNum = prevNum = newNum = skippedOp = prevOp = newOp = 0;
+}
+
+function debug () {
+    console.log(`skippedNum: ${skippedNum} \n skippedOp: ${skippedOp} \n prevNum: ${prevNum} \n prevOp: ${prevOp} \n newNum: ${newNum} \n newOp: ${newOp}`);
 }
